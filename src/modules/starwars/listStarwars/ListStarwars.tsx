167e6dfd8cd456b66bbef71ea1792c8107a14836
Hasanli Hajagha <hacagahasanli@gmail.com>
2025-11-07 17:22:52 +0400

    feat(title): on list star wars page

--- src/modules/starwars/listStarwars/ListStarwars.tsx
+++ src/modules/starwars/listStarwars/ListStarwars.tsx
@@ -3,6 +3,7 @@ import Show from "~/shared/ui/Show/Show";
 import StarwarsCard from "./ui/StarwarsCard/StarwarsCard";
 import useCharactersQuery from "./api/queries/useCharactersQuery";
 import StarwarsCardSkeleton from "./ui/StarwarsCardSkeleton/StarwarsCardSkeleton";
+import Col from "~/shared/ui/Col/Col";
 
 const ListStartWars = () => {
   const {
@@ -16,16 +17,21 @@ const ListStartWars = () => {
   const isNotEmpty = !!charactersList?.length;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-      <Show when={isCharactersFetching}>
-        <StarwarsCardSkeleton />
-      </Show>
-      <Show when={!isCharactersFetching && isCharactersSucceed && isNotEmpty}>
-        {charactersList?.map((character, idx) => (
-          <StarwarsCard id={idx + 1} character={character} />
-        ))}
-      </Show>
-    </div>
+    <Col>
+      <h1 className="text-yellow-500 animate-pulse font-extrabold mb-4 text-shadow-md text-shadow-yellow-700 w-max text-5xl">
+        Star Wars
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+        <Show when={isCharactersFetching}>
+          <StarwarsCardSkeleton />
+        </Show>
+        <Show when={!isCharactersFetching && isCharactersSucceed && isNotEmpty}>
+          {charactersList?.map((character, idx) => (
+            <StarwarsCard id={idx + 1} character={character} />
+          ))}
+        </Show>
+      </div>
+    </Col>
   );
 };
 
